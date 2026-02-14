8f112c0607445973c34fd96d7db714c9e8573158
IFA-01 <bogatyr.oleg@yahoo.com>
2026-02-14 12:53:48 +0300

    Prettier warns corrected

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  collectCoverageFrom: ['src/**/*.{js,ts}', '!**/*.test.{js,ts}', '!**/node_modules/**'],
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!**/*.test.{js,ts}',
+    '!**/node_modules/**',
+  ],
   coverageThreshold: {
     global: {
       branches: 70,
@@ -27,15 +31,18 @@ module.exports = {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      useESM: true,
-      tsconfig: {
-        esModuleInterop: true,
-        allowSyntheticDefaultImports: true,
-        module: 'ES2020',
-        moduleResolution: 'node',
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+          module: 'ES2020',
+          moduleResolution: 'node',
+        },
       },
-    }],
+    ],
     '^.+\\.js$': 'babel-jest',
   },
   extensionsToTreatAsEsm: ['.ts'],
